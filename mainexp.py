4b4fd39860db6b64ec91c23390317dbe60a8995f
chaim-duchovny <chaim.duchovny@gmail.com>
2025-01-06 00:39:47 +0200

    Update mainexp.py

--- mainexp.py
+++ mainexp.py
@@ -11,12 +11,13 @@ class Main:
         self.game = Game(self.boardgame)
         self.phase = "placement"
         self.current_player = "red"
+        self.game_over = False
+        self.win_message = None
 
     def mainloop(self):
         screen = self.screen
         game = self.game
         running = True
-
         while running:
             game.show_bg(screen)
             game.show_pieces(screen)
@@ -25,14 +26,16 @@ class Main:
             game.render_number_of_black_pieces(screen)
             game.render_number_of_red_pieces(screen)
 
+            if self.game_over:
+                self.display_win_message(screen)
+
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
-                elif event.type == pygame.MOUSEBUTTONDOWN:
+                elif event.type == pygame.MOUSEBUTTONDOWN and not self.game_over:
                     pos = pygame.mouse.get_pos()
                     col = pos[0] // RECT_WIDTH
                     row = pos[1] // RECT_HEIGHT
-
                     if self.phase == "placement":
                         self.handle_placement(event.button, row, col)
                     else:
@@ -51,7 +54,7 @@ class Main:
             self.boardgame.reset_piece_placement()
         elif button == 3:
             self.boardgame.return_piece_black_selction(row, col)
-            self.boardgame.return_piece_black_return(row, col)  
+            self.boardgame.return_piece_black_return(row, col)
             self.boardgame.return_piece_red_selction(row, col)
             self.boardgame.return_piece_red_return(row, col)
 
@@ -65,11 +68,22 @@ class Main:
                 self.boardgame.select_piece(row, col, self.current_player)
             else:
                 start_row, start_col = self.boardgame.selected_piece
-                if self.boardgame.move_piece(start_col, start_row, col, row, self.current_player):
-                    self.current_player = "black" if self.current_player == "red" else "red"
-                self.boardgame.selected_piece = None
-        self.game.show_pieces(self.screen)
-        pygame.display.update()
+                move_result = self.boardgame.move_piece(start_col, start_row, col, row, self.current_player)
+                if move_result:
+                    if isinstance(move_result, tuple) and move_result[1]:
+                        self.game_over = True
+                        self.win_message = move_result[1]
+                    else:
+                        self.current_player = "black" if self.current_player == "red" else "red"
+                    self.boardgame.selected_piece = None
+                    self.game.show_pieces(self.screen)
+                    pygame.display.update()
+
+    def display_win_message(self, surface):
+        font = pygame.font.Font(None, 36)
+        text = font.render(self.win_message, True, (255, 0, 0))
+        text_rect = text.get_rect(center=(WIDTH // 2, HEIGHT // 2))
+        surface.blit(text, text_rect)
 
 main = Main()
 main.mainloop()
